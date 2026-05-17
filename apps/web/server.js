5767d8847838f1ce904b121cccf4898699d8b8b3
Shajpori <gazimiraz@gmail.com>
2026-05-17 18:36:18 +0600

    fix: add dir:__dirname to Next.js server so it finds .next from any CWD

--- apps/web/server.js
+++ apps/web/server.js
@@ -1,9 +1,10 @@
+const path = require('path');
 const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
-const app = next({ dev: false });
+const app = next({ dev: false, dir: __dirname });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
